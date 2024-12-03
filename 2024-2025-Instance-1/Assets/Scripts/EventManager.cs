79dded0ebe5c5c3e8b2c4ac82fdcc4a42f1b966d
shenanigan11 <enzo2dessaint@gmail.com>
2024-12-03 14:30:42 +0100

    EventManager
    Contains all the event we could use

---
+++ 2024-2025-Instance-1/Assets/Scripts/EventManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// For add an event call : EventManager.Instance.Event.AddListener(Function);
+/// The function should have the shape : UnityEvent<params> take Function(params)
+/// For call an event just add this line : EventManager.Instance.Event.Invoke(params);
+/// this will call all function you have add to your event
+/// </summary>
+
+public class EventManager : MonoBehaviour
+{
+    private static EventManager _instance;
+    public static EventManager Instance {  get { return _instance; } }
+
+    public UnityEvent<GameObject> AddRuneToInventory; // TODO Rune
+    public UnityEvent<Vector2> OnMove;
+    public UnityEvent OnInteract;
+    public UnityEvent OnPause;
+    public UnityEvent<float> UpdateTimer;
+    public UnityEvent<int> UpdateLife;
+    public UnityEvent<GameObject> UpdateRune;
+    public UnityEvent UpdateDeath;
+    public UnityEvent OnDeath;
+    private void Start()
+    {
+        if (_instance == null) 
+        {
+            _instance = this;
+            return;
+        }
+        Destroy(this);
+    }
+}
