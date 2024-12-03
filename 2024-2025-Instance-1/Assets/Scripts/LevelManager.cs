b7a8919c6fb0ac4102e4a0c7f11fbb1b60c2cfea
Elckarow <101005497+Elckarow@users.noreply.github.com>
2024-12-03 14:42:46 +0100

    create LevelManager file
    and the skeleton of the class

---
+++ 2024-2025-Instance-1/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    private static LevelManager _instance;
+
+    [Tooltip("Maximum number of deaths allowed. 0 means unlimited deaths.")]
+    [Range(0, 100)] public int maxDeath = 0;
+
+    public int currentDeath = 0;
+
+    private void Awake()
+    {
+        if (_instance)
+        {
+            Destroy(gameObject);
+            return;
+        } 
+        _instance = this;
+    }
+
+    public void Retry()
+    {
+
+    }
+
+    public void Win()
+    {
+
+    }
+
+    public void Lose()
+    {
+
+    }
+
+    public void Pause()
+    {
+
+    }
+
+    public void UpdateTimer()
+    {
+
+    }
+
+    public void OnDeath()
+    {
+        if (maxDeath != 0)
+        {
+            if (++currentDeath == maxDeath)
+            {
+                Debug.Log("TODO maximum number of deaths reached");
+            }
+        }
+    }
+}
