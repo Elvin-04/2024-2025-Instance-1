886fff345e4c638d5352e0dbcbac898658d33798
Elckarow <101005497+Elckarow@users.noreply.github.com>
2024-12-05 13:54:21 +0100

    Update EventManager.cs

--- 2024-2025-Instance-1/Assets/Scripts/EventManager.cs
+++ 2024-2025-Instance-1/Assets/Scripts/EventManager.cs
@@ -11,14 +11,14 @@ using UnityEngine.Events;
 public class EventManager : MonoBehaviour
 {
     public static EventManager Instance { get; private set; }
-    public UnityEvent<GameObject> AddRuneToInventory { get; private set; } // TODO Rune
+    public UnityEvent<Rune> AddRuneToInventory { get; private set; } // TODO Rune
     public UnityEvent<Vector2> OnMoveStarted  { get; private set; }
     public UnityEvent OnMoveCanceled { get; private set; }
     public UnityEvent OnInteract { get; private set; }
     public UnityEvent OnPause { get; private set; }
 
     public UnityEvent<int> UpdateLife { get; private set; }
-    public UnityEvent<GameObject> UpdateRune { get; private set; }
+    public UnityEvent<Rune> UpdateRune { get; private set; }
     public UnityEvent UpdateDeath { get; private set; }
     public UnityEvent OnDeath { get; private set; }
     public UnityEvent UpdateClock { get; private set; }
