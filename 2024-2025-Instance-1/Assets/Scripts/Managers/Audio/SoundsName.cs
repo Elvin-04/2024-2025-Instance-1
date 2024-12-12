bd2394e588ed43d901cd7d47f405bb5ccba62ad1
kalderlunt <kalderlunt@gmail.com>
2024-12-12 11:51:51 +0100

    enum ajouter
    enum :
    - SoundName

---
+++ 2024-2025-Instance-1/Assets/Scripts/Managers/Audio/SoundsName.cs
@@ -0,0 +1,20 @@
+namespace Managers.Audio
+{
+    public enum SoundsName
+    {
+        ActiveArrowThrower,
+        ActivateSpike,
+        DeactivateSpike,
+        Collectible,
+        SandMovementPlayer,
+        ButtonPress,
+        ButtonRelease,
+        DeathByArrow,
+        BreakDoor,
+        OpenDoor,
+        CloseDoor,
+        PressPressurePlate,
+        ReleasePressurePlate,
+        DestroyWall
+    }
+}
