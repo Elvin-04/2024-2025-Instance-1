fde49e6441421cb6e982e537a692836e3bdfad7d
Elckarow <101005497+Elckarow@users.noreply.github.com>
2024-12-16 10:21:27 +0100

    add spawn points

--- 2024-2025-Instance-1/Assets/Scripts/Managers/LevelManager.cs
+++ 2024-2025-Instance-1/Assets/Scripts/Managers/LevelManager.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using Grid;
+using System.Collections.Generic;
 using UnityEngine.Assertions;
 using UnityEngine.SceneManagement;
 
@@ -8,13 +10,26 @@ namespace Managers
     {
         private static LevelManager _instance;
 
-        [field: SerializeField] public Transform spawnPoint { get; private set; }
+        [SerializeField] private GridManager _gridManager;
+
         [field: SerializeField] public ScoreCounter maxDeaths { get; private set; }
         [field: SerializeField] public ScoreCounter maxSteps { get; private set; }
 
+        [Tooltip("Order matters! Once a spawn point is reached, any previous spawn points will be disabled.")]
+        [SerializeField] private List<Transform> spawnPoints = new List<Transform>();
+        private List<(Vector2Int, Vector3)> _spawnPoints = new List<(Vector2Int, Vector3)>();
+
+        private int _spawnPointIndex = 0;
+
+        public Vector3 spawnPoint
+        { // ugly
+            get => _spawnPoints.Count == 0 ? spawnPoints[0].position : _spawnPoints[_spawnPointIndex].Item2;
+        }
+
         private void Awake()
         {
-            Assert.IsNotNull(spawnPoint, "spawnPoint is null in LevelManager");
+            if (spawnPoints.Count == 0)
+                throw new System.Exception("no spawn points");
         }
 
         private void Start()
@@ -27,12 +42,38 @@ namespace Managers
 
             _instance = this;
             EventManager.instance.onRetry.AddListener(ReloadScene);
+            EventManager.instance.onPlayerMoved.AddListener(OnPlayerMoved);
+
+            foreach (Transform spawnPoint in spawnPoints)
+            {
+                Vector3 spawnPos = _gridManager.GetCellPos(spawnPoint.position);
+                _spawnPoints.Add((_gridManager.GetCellIndex(spawnPos), spawnPos));
+            }
+        }
+
+        public void OnPlayerMoved(Vector3 position)
+        {
+            if (_spawnPointIndex == spawnPoints.Count)
+                return;
+
+            Vector2Int cellIndex = _gridManager.GetCellIndex(_gridManager.GetCellPos(position));
+
+            for (int i = 0; i < _spawnPoints.Count; i++)
+            {
+                if (_spawnPoints[i].Item1 == cellIndex && i > _spawnPointIndex)
+                {
+                    _spawnPointIndex = i;
+                    break;
+                }
+            }
         }
 
         private void OnDrawGizmos()
         {
             Gizmos.color = Color.grey;
-            Gizmos.DrawCube(spawnPoint.position, Vector3.one * 0.5f);
+
+            foreach (Transform spawnPoint in spawnPoints)
+                Gizmos.DrawCube(spawnPoint.position, Vector3.one * 0.5f);
         }
 
         public void ReloadScene()
