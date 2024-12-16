d68d618938423fbed00be49b85488632abd953c7
DoriannX <theodor.xav@gmail.com>
2024-12-16 15:13:42 +0100

    fixing poison

--- 2024-2025-Instance-1/Assets/Scripts/Traps/Poison/PoisonTrap.cs
+++ 2024-2025-Instance-1/Assets/Scripts/Traps/Poison/PoisonTrap.cs
@@ -7,36 +7,14 @@ namespace Traps
 
 public class PoisonTrap : CellObjectBase, IInteractable, IWeightInteractable
 {
-    [SerializeField] private int _maxTickClock;
-    private int _currentTickClock;
-    private bool _playerPoisoned = false;
 
     [HideInInspector] public PoisonTrapCreator creator;
 
     public bool canPickUp { get => false; set { } }
 
-    private void Start()
-    {
-        EventManager.instance.onDeath?.AddListener(OnDeath); 
-        EventManager.instance.onClockUpdated?.AddListener(OnClockUpdate);
-    }
-
-    public void OnClockUpdate()
-    {
-        if (!_playerPoisoned)
-            return;
-
-        // yes, i++
-        //  not ++i
-        if (_currentTickClock++ >= _maxTickClock)
-        {
-            EventManager.instance.onDeath?.Invoke();
-        }
-    }
-
     public void Interact()
     {
-        _playerPoisoned = true;
+        creator.PoisonPlayer();
     }
 
     public void StopInteract() {}
@@ -49,12 +27,6 @@ public class PoisonTrap : CellObjectBase, IInteractable, IWeightInteractable
     public void StopWeightInteract()
     {
         creator.StopWeightInteract(this);
-    }
-
-    private void OnDeath()
-    {
-        _currentTickClock = 0;
-        _playerPoisoned = false;
     }
 }
 
