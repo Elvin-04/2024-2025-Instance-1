85cf553c90ce9ec2d9acfbc3740299b0905cc50b
DoriannX <theodor.xav@gmail.com>
2024-12-17 15:49:51 +0100

    adding controller navigation

--- 2024-2025-Instance-1/Assets/Scripts/Menu/MainMenu.cs
+++ 2024-2025-Instance-1/Assets/Scripts/Menu/MainMenu.cs
@@ -1,13 +1,26 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace Menu
 {
     public class MainMenu : MonoBehaviour
     {
+        [SerializeField] private GameObject _mainMenuFirstBtn, _optionFirstBtn;
         public void Play()
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        public void OpenOption()
+        {
+            EventSystem.current.SetSelectedGameObject(_optionFirstBtn);
+        }
+
+        public void GoToMainMenu()
+        {
+            EventSystem.current.SetSelectedGameObject(_mainMenuFirstBtn);
         }
 
         public void Quit()
