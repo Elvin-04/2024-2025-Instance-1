9dbcc252c3196575279cac62e38dc24626876747
kalderlunt <kalderlunt@gmail.com>
2024-12-18 11:06:33 +0100

    feat-sfxButtonScript

---
+++ 2024-2025-Instance-1/Assets/Scripts/Audio/ButtonScript/ButtonAudioController.cs
@@ -0,0 +1,65 @@
+using Managers.Audio;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonAudioController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    [SerializeField] private SoundsName _mouseOver;
+    [SerializeField] private SoundsName _mouseExit;
+    [SerializeField] private SoundsName _mouseClick;
+
+    private Button _button;
+
+    //[Header("Gamepad")]
+    private bool _isSelected;
+
+    private void Awake()
+    {
+        _button = GetComponent<Button>();
+        _button.onClick.AddListener(ButtonClick);
+    }
+
+    private void Update()
+    {
+        if (EventSystem.current.currentSelectedGameObject == gameObject && !_isSelected)
+        {
+            _isSelected = true;
+            OnGamepadSelect();
+        }
+        else if (EventSystem.current.currentSelectedGameObject != gameObject && _isSelected)
+        {
+            _isSelected = false;
+            OnGamepadDeselect();
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        EventManager.instance.onPlaySfx?.Invoke(_mouseOver);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        EventManager.instance.onPlaySfx?.Invoke(_mouseExit);
+    }
+
+    private void OnGamepadSelect()
+    {
+        EventManager.instance.onPlaySfx?.Invoke(_mouseOver);
+    }
+
+    private void OnGamepadDeselect()
+    {
+        EventManager.instance.onPlaySfx?.Invoke(_mouseExit);
+    }
+
+    private void ButtonClick()
+    {
+        EventManager.instance.onPlaySfx?.Invoke(_mouseClick);
+    }
+}
